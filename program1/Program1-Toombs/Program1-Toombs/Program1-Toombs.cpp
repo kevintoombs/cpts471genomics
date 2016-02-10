37ac01f5d2fdcb133d635bde2b15085ff2751f5a
Kevin <toombs.kevin@gmail.com>
2016-02-09 20:47:16 -0800

    program 1 setup init

---
+++ program1/Program1-Toombs/Program1-Toombs/Program1-Toombs.cpp
@@ -0,0 +1,22 @@
+// Program1-Toombs.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+struct config {
+	int matchScore;
+	int mismatchScore;
+	int startGapScore;
+	int continueGapScore;
+};
+
+struct DP_cell {
+	int score;
+};
+
+int main()
+{
+	DP_cell testcell;
+    return 0;
+}
+
